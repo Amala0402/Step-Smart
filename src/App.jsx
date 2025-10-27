66c4cecd52ffe339428fb4beef41368c79afae41
Amala0402 <kanaparthiamala226@gmail.com>
2025-10-27 10:54:59 +0530

    Add my folder/files

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+import { Box } from "@chakra-ui/react";
+import AllRoutes from "./components/AllRoutes";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <Box w="80%" mx="auto">
+      <Navbar />
+      <AllRoutes />
+      <Footer />
+    </Box>
+  );
+}
+
+export default App;
---
+++ src/components/Homepage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Midpage from "./Midpage";
+import Slider from "./Swiper";
+
+const Homepage = () => {
+  return (
+    <div>
+      <Slider />
+      <Midpage />
+    </div>
+  );
+};
+
+export default Homepage;
